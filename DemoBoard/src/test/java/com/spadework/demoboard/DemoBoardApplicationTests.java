8d615f576f5674beb8b72b81e20bb2ac2a6eed95
dyyoon <dyyoon@anchors-biz.com>
2021-12-09 15:15:23 +0900

    Boot Board initial..!!

---
+++ DemoBoard/src/test/java/com/spadework/demoboard/DemoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spadework.demoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
